e80b3d3a63337614cc25712cba1ba9d90ba960d4
Alberto José <albertorauljose2@gmail.com>
2026-04-08 15:25:49 +0100

    feat(ui/input): add input component with stories

---
+++ src/components/ui/input/input.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { userEvent, within } from 'storybook/test';
+
+import { Input } from '@/components/ui';
+
+type StoryProps = React.ComponentProps<typeof Input>;
+
+const meta = {
+  title: 'ui/Input',
+  component: Input,
+  tags: ['autodocs'],
+  args: {
+    placeholder: 'Insert your name',
+  },
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<StoryProps>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Focused: Story = {
+  name: 'State / Focused',
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('textbox'));
+  },
+};
+
+export const Invalid: Story = {
+  name: 'State / Invalid',
+  args: {
+    isInvalid: true,
+  },
+};
+
+export const InvalidWithValue: Story = {
+  name: 'State / Invalid with value',
+  args: {
+    isInvalid: true,
+    value: 'John Doe',
+  },
+};
+
+export const Disabled: Story = {
+  name: 'State / Disabled',
+  args: {
+    disabled: true,
+  },
+};
+
+export const DisabledWithValue: Story = {
+  name: 'State / Disabled with value',
+  args: {
+    disabled: true,
+    value: 'John Doe',
+  },
+};
---
+++ src/components/ui/input/input.tsx
@@ -0,0 +1,75 @@
+import { cva } from 'class-variance-authority';
+import type { InputHTMLAttributes } from 'react';
+
+import { cn } from '@/libs/cn';
+
+const inputVariants = cva(
+  cn(
+    'px-6',
+    'py-4.5',
+    'bg-white',
+    'border-2',
+    'border-solid',
+    'rounded-lg',
+    'font-display',
+    'font-bold',
+    'text-[0.875rem]',
+    '-tracking-[0.015625em]',
+    'text-black',
+    'placeholder:text-black-o-40',
+
+    'disabled:bg-gray-200',
+    'disabled:border-gray-400',
+    'disabled:opacity-50',
+    'disabled:placeholder:text-black-o-50',
+    'disabled:cursor-not-allowed',
+    'disabled:pointer-events-none',
+    'disabled:select-none',
+
+    'focus:outline-2',
+    'focus:outline-solid',
+  ),
+  {
+    variants: {
+      isInvalid: {
+        false: cn(
+          'border-gray-600',
+          'focus:outline-primary-400',
+          'focus:caret-primary-400',
+          'focus-visible:outline-primary-400',
+          'focus-visible:caret-primary-400',
+        ),
+
+        true: cn(
+          'border-danger-400',
+          'focus:outline-danger-400',
+          'focus:caret-danger-400',
+          'focus-visible:outline-danger-400',
+          'focus-visible:caret-danger-400',
+        ),
+      },
+    },
+
+    defaultVariants: {
+      isInvalid: false,
+    },
+  },
+);
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isInvalid?: boolean;
+}
+
+const Input = ({ isInvalid, className, ...props }: InputProps) => {
+  const inputClasses = cn(inputVariants({ isInvalid }), className);
+
+  return (
+    <input
+      aria-invalid={isInvalid ?? undefined}
+      className={inputClasses}
+      {...props}
+    />
+  );
+};
+
+export default Input;
