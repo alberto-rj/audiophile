a9c17aed619e8e57dd6e78f9e2e8bfea02b683b3
Alberto José <albertorauljose2@gmail.com>
2026-04-12 12:59:00 +0100

    feat(widgets/Hero): add hero component with stories

---
+++ src/components/widgets/hero/hero.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import { Hero } from '@/components/widgets';
+import { newProduct, oldProduct } from '@/libs/mocks';
+
+type StoryProps = React.ComponentProps<typeof Hero>;
+
+const meta = {
+  title: 'widgets/Hero',
+  component: Hero,
+} satisfies Meta<StoryProps>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    product: newProduct,
+  },
+};
+
+export const WithOldProduct: Story = {
+  args: {
+    product: oldProduct,
+  },
+};
