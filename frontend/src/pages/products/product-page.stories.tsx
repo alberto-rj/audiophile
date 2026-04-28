8c68d727cf5fbd6f6c6ad897648aa7e4588414b7
Alberto José <albertorauljose2@gmail.com>
2026-04-28 15:10:11 +0100

    refactor(product-page): create separate stories for products by category

--- frontend/src/pages/products/product-page.stories.tsx
+++ frontend/src/pages/products/product-page.stories.tsx
@@ -19,9 +19,16 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const XX59Headphones: Story = {
-  name: 'Headphones / XX59',
+  name: 'Headphones / XX99',
   parameters: {
-    route: createProductRoute('xx59-headphones'),
+    route: createProductRoute('xx99-mark-two-headphones'),
+  },
+};
+
+export const XX99Headphones: Story = {
+  name: 'Speakers / ZX7',
+  parameters: {
+    route: createProductRoute('zx7-speaker'),
   },
 };
 
@@ -29,12 +36,5 @@ export const YX1Earphones: Story = {
   name: 'Earphones / YX1',
   parameters: {
     route: createProductRoute('yx1-earphones'),
-  },
-};
-
-export const XX99Headphones: Story = {
-  name: 'Headphones / XX99 Mark Two',
-  parameters: {
-    route: createProductRoute('xx99-mark-two-headphones'),
   },
 };
