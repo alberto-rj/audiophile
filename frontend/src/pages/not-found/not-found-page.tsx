f3caea81fdebe93abe361894b0168c743fe6244b
Alberto José <albertorauljose2@gmail.com>
2026-05-01 20:05:40 +0100

    feat(not-found): add structured 404 page with homepage redirect

--- frontend/src/pages/not-found/not-found-page.tsx
+++ frontend/src/pages/not-found/not-found-page.tsx
@@ -1,5 +1,72 @@
+import { Link } from 'react-router-dom';
+
+import { Button } from '@/components/ui';
+import { cn } from '@/libs/cn';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <main
+      className={cn(
+        'region',
+        'min-block-screen',
+        'flex',
+        'justify-center',
+        'items-center',
+      )}
+    >
+      <div className={cn('wrapper')}>
+        <div
+          className={cn(
+            'flex',
+            'flex-col',
+            'items-center',
+            'gap-7',
+
+            'md:gap-10',
+
+            'lg:items-start',
+          )}
+        >
+          <div
+            className={cn(
+              'max-inline-140',
+              'flex',
+              'flex-col',
+              'items-center',
+              'text-center',
+              'gap-6',
+
+              'lg:text-start',
+              'lg:items-start',
+            )}
+          >
+            <h1
+              className={cn(
+                'text-2xl',
+
+                'md:text-4xl',
+
+                'text-black',
+                'uppercase',
+              )}
+            >
+              Oops! We can’t seem to find that page
+            </h1>
+            <p className={cn('max-inline-116')}>
+              It looks like the link you followed might be broken or the page
+              has been moved. Don't worry, it happens to the best of us!
+            </p>
+          </div>
+          <Button
+            variant={'primary'}
+            asChild
+          >
+            <Link to='/'>Back to Homepage</Link>
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default NotFoundPage;
