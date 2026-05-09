141fa02b56ffb228e96ad4db83d5ad735a9a8622
Alberto José <albertorauljose2@gmail.com>
2026-05-09 21:00:48 +0100

    feat(order-details): handle loading and error states

--- frontend/src/pages/order-details/order-details-page.tsx
+++ frontend/src/pages/order-details/order-details-page.tsx
@@ -1,5 +1,76 @@
+import { useParams } from 'react-router-dom';
+
+import { useGetOrderByIdQuery } from '@/app/services/orders-api';
+import { ErrorMessage, GoBack } from '@/components/widgets';
+import { cn } from '@/libs/cn';
+import { Spinner } from '@/components/ui';
+
 const OrderDetailsPage = () => {
-  return <div>Order Details Page</div>;
+  const slug = useParams()?.slug;
+  const orderId = parseInt(slug!);
+
+  const { isLoading, isError, refetch } = useGetOrderByIdQuery(orderId!, {
+    skip: typeof orderId === 'undefined',
+  });
+
+  if (isLoading) {
+    return (
+      <Spinner
+        aria-label='Loading order...'
+        className={cn('mx-auto')}
+      />
+    );
+  }
+
+  if (isError) {
+    return (
+      <ErrorMessage>
+        <ErrorMessage.Description>
+          Failed to load order.
+        </ErrorMessage.Description>
+        <ErrorMessage.Retry
+          onClick={refetch}
+          aria-label='Try again - reload order'
+        >
+          Try again
+        </ErrorMessage.Retry>
+      </ErrorMessage>
+    );
+  }
+
+  return (
+    <>
+      <GoBack>
+        <GoBack.Control />
+      </GoBack>
+      <div
+        className={cn(
+          'region-end',
+          'wrapper',
+          'flex',
+          'flex-col',
+          'items-center',
+          'gap-8',
+
+          'md:gap-24',
+        )}
+      >
+        <h1
+          className={cn(
+            'text-center',
+            'text-xl',
+
+            'lg:text-2xl',
+
+            'text-black',
+            'uppercase',
+          )}
+        >
+          Order details page
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default OrderDetailsPage;
